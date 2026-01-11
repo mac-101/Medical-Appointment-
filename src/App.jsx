1ba952b7ec8350f9a5177a2c3aa10c5e181a92ee
Charles MacAnthony <charlesmacanthony797@gmail.com>
2026-01-11 12:25:41 -0800

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/home.jsx'
+import Profile from './pages/profile_id.jsx'
+import Search from './pages/search.jsx'
+import DashboardPatient from './pages/dashboard_patient.jsx'
+import DashboardProvider from './pages/dashboard_Provider.jsx'
+import SignUp from '.pages/registration.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/dashboard/patient" element={<DashboardPatient />} />
+          <Route path="/dashboard/provider" element={<DashboardProvider />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-
 export default App
